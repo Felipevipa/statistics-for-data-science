5b05694a89d63031d6bebb2b76a63cec0366f9bd
unknown <felipevipa@outlook.com>
2022-10-08 15:27:54 -0500

    salaries_and_experience

--- 1.1/friendship_dict.py
+++ 1.1/friendship_dict.py
@@ -40,3 +40,96 @@ num_users = len(users)
 avg_connections = total_connections / num_users
 
 print(avg_connections)
+
+# Create a list (user_id, number_of_friends).
+num_friends_by_id = [(user["id"], number_of_friends(user))
+                     for user in users]
+
+num_friends_by_id.sort(                                # Sort the list
+       key=lambda id_and_friends: id_and_friends[1],   # by num_friends
+       reverse=True)                                   # largest to smallest
+
+# Each pair is (user_id, num_friends):
+# [(1, 3), (2, 3), (3, 3), (5, 3), (8, 3),
+#  (0, 2), (4, 2), (6, 2), (7, 2), (9, 1)]
+
+
+def foaf_ids_bad(user):
+    """foaf is short for "friend of a friend" """
+    return([foaf_id 
+            for friend_id in friendships[user["id"]]
+            for foaf_id in friendships[friend_id]])
+
+        
+print(friendships[0])  # [1, 2]
+print(friendships[1])  # [0, 2, 3]
+print(friendships[2])  # [0, 1, 3]
+
+from collections import Counter     # not loaded by default
+
+def friends_of_friends(user):
+    user_id = user["id"]
+    return(Counter(
+        foaf_id
+        for friend_id in friendships[user_id]   # For each of my friends,
+        for foaf_id in friendships[friend_id]   # find their friends
+        if foaf_id != user_id                   # who aren't me
+        and foaf_id not in friendships[user_id] # and aren't my friends.
+    ))
+
+print(friends_of_friends(users[3]))             # Counter({0: 2, 5: 1})
+
+
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+def data_scientist_who_like(target_interest):
+    """Find the ids of all users who like the target interest"""
+    return([user_id 
+            for user_id, user_interest in interests
+            if user_interest == target_interest])
+
+from collections import defaultdict
+
+# Keys are interests, values are list of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+# Keys are user_id,v alues are lists of interests for that user_id
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+
+# print(user_ids_by_interest)
+# print(interests_by_user_id)
+
+
+def most_common_interests_with(user):
+    return(Counter(
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
+    ))
+
+# print(most_common_interests_with(users[0]))
+
